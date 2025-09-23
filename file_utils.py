2679db20981f7fb452b690d092b679e3c14814db
Imam Umar Muhammad <150058490+imamumhd@users.noreply.github.com>
2025-09-23 17:06:18 +0100

    Update file_utils.py

--- file_utils.py
+++ file_utils.py
@@ -1,13 +1,14 @@
 def save_to_file(filename, text):
-    file = open(filename, "w")
-    file.write(text)
+    file = open(filename, "a")
+    file.write(text + "\n")
     file.close()
-    print("File saved!")
+    print("Text saved!")
 
 
 def read_from_file(filename):
     file = open(filename, "r")
     content = file.read()
     file.close()
+    print("File content:")
     print(content)
     return content
